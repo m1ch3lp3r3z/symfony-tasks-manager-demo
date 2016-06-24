72392fa6eb220da71f5a5895d1b311288b95d5ba
Michel Perez <michel.perez8402@gmail.com>
2016-06-24 00:59:15 -0600

    Layout not in use for now, removes inner code

--- web/assets/js/src/views/layout.js
+++ web/assets/js/src/views/layout.js
@@ -3,49 +3,10 @@ define(function(require) {
 
     var _ = require('underscore'),
         Backbone = require('backbone'),
-        Mustache = require('mustache'),
-        template = require('text!templates/home.html'),
-        TasksView = require('views/task/collection');
+        Mustache = require('mustache');
 
     require('backbone.marionette');
 
     return Backbone.Marionette.LayoutView.extend({
-        template: Mustache.parse(template),
-        collectionView: null,
-
-        regions: {
-            shows: '#postercollection'
-        },
-
-        ui: {
-            pageNum: '#pagenum .value',
-            nextPage: '#navcontrols #navright',
-            prevPage: '#navcontrols #navleft',
-            carousel: '#postercollection'
-        },
-
-        events: {
-            'click #navleft': '_onNavLeftClick',
-            'click #navright': '_onNavRightClick',
-            'click #navicons #navicondetails': '_onNavIconDetailsClick',
-            'click #navicons #naviconthumbs': '_onNavIconDetailsClick'
-        },
-
-        initialize: function(options) {
-            _.bindAll(this, '_onCollectionViewRender');
-        },
-
-        _onCollectionViewRender: function() {
-            if(this.collectionView.collection.length == 0) {
-                this.currentPage = 0;
-                this.totalPages = 0;
-            } else {
-
-            }
-            this._onCarouselSlid();
-            this._updatePrevNext();
-            this.$('#navcontrols').css('display', this.totalPages == 0 ? 'none' : 'block');
-            this.delegateEvents();
-        },
     });
 });
